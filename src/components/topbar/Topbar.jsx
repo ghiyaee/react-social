22a6421e3e1debe4201da185aa4647d32f533104
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-05-11 09:32:05 +0430

    add sidebar feed rightbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -7,34 +7,42 @@ import { Chat } from "@mui/icons-material";
 export default function Topbar() {
   return (
     <div className="topbarContainer">
+
       <div className="topbarLeft">
         <span className="logo"> logo Socail</span>
       </div>
+
       <div className="topbarCenter">
         <div className="searchbar">
           <SearchIcon className="searchIcon"/>
           <input placeholder="Search Bar" className="searchInput" />
         </div>
       </div>
+
       <div className="topbarRight">
+
         <div className="topbarLinks">
           <span className="topbarLink">HomePage</span>
           <span className="topbarLink">TimeLine</span>
         </div>
+
         <div className="topbarIcons">
           <div className="topbarIconItem">
             <PersonIcon />
             <span className="topbarIconBadge">1</span>
           </div>
+
           <div className="topbarIconItem">
             <ChatIcon/>
             <span className="topbarIconBadge">1</span>
           </div>
+
           <div className="topbarIconItem">
             <NotificationsIcon />
             <span className="topbarIconBadge">1</span>
           </div>
         </div>
+        
         <img src="/assets/person/img1.jpg" alt="" className="topbarImg" />
       </div>
     </div>
