de1ddef621329ee18f834c362719af219e8706c4
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-05-12 11:55:46 +0430

    update post

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,10 +1,19 @@
 import "./post.css"
-
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 export default function Post() {
   return (
     <div className="post">
       <div className="postWrapper">
-        <div className="postTop"></div>
+        <div className="postTop">
+                  <div className="postTopLeft">
+                      <img src="/assets/person/img1.jpg" alt="" className="postProfileImg" />
+                      <span className="postUsername">farnoosh</span>
+                      <span className="postDate">5 mins age</span>
+                 </div>
+                  <div className="postTopRight">
+                   <MoreVertIcon/>   
+                 </div>
+           </div>
         <div className="postCenter"></div>
         <div className="postBottom"></div>
       </div>
