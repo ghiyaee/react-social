bc0aef81366f95e18679ba99a2d350960902e424
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-05-12 14:56:17 +0430

    add Rightbar

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,6 +2,15 @@ import "./rightbar.css"
 
 export default function Rightbar() {
   return (
-    <div className="rightbar" >Rightbar</div>
-  )
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img src="/assets/person/gift1.png" alt="" className="birthdayImg" />
+          <span className="birthdayText">
+            <b>farnoosh</b> and <b>two friends</b> have a brithday today
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
